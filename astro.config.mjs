3a1c31a311480a00dcb6682eb742f4f4101438ec
Mkrari <917035358@qq.com>
2026-08-10 19:36:52 +0800

    v7.2.2 scurity upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import sitemap from "@astrojs/sitemap";
-import { unified } from "@astrojs/markdown-remark";
 import svelte, { vitePreprocess } from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import swup from "@swup/astro";
@@ -36,6 +36,9 @@ export default defineConfig({
 	output: "static",
 
 	integrations: [
+		tailwind({
+			nesting: true,
+		}),
 		swup({
 			theme: false,
 			animationClass: "transition-swup-",
@@ -116,58 +119,54 @@ export default defineConfig({
 		sitemap(),
 	],
 	markdown: {
-		processor: unified({
-			remarkPlugins: [
-				remarkMath,
-				remarkContent,
-				remarkGithubAdmonitionsToDirectives,
-				remarkDirective,
-				remarkSectionize,
-				parseDirectiveNode,
-				remarkMermaid,
-			],
-			rehypePlugins: [
-				rehypeKatex,
-				rehypeSlug,
-				rehypeWrapTable,
-				rehypeMermaid,
-				rehypeImageWidth,
-				[
-					rehypeComponents,
-					{
-						components: {
-							github: GithubCardComponent,
-							note: (x, y) => AdmonitionComponent(x, y, "note"),
-							tip: (x, y) => AdmonitionComponent(x, y, "tip"),
-							important: (x, y) =>
-								AdmonitionComponent(x, y, "important"),
-							caution: (x, y) =>
-								AdmonitionComponent(x, y, "caution"),
-							warning: (x, y) =>
-								AdmonitionComponent(x, y, "warning"),
-						},
+		remarkPlugins: [
+			remarkMath,
+			remarkContent,
+			remarkGithubAdmonitionsToDirectives,
+			remarkDirective,
+			remarkSectionize,
+			parseDirectiveNode,
+			remarkMermaid,
+		],
+		rehypePlugins: [
+			rehypeKatex,
+			rehypeSlug,
+			rehypeWrapTable,
+			rehypeMermaid,
+			rehypeImageWidth,
+			[
+				rehypeComponents,
+				{
+					components: {
+						github: GithubCardComponent,
+						note: (x, y) => AdmonitionComponent(x, y, "note"),
+						tip: (x, y) => AdmonitionComponent(x, y, "tip"),
+						important: (x, y) =>
+							AdmonitionComponent(x, y, "important"),
+						caution: (x, y) => AdmonitionComponent(x, y, "caution"),
+						warning: (x, y) => AdmonitionComponent(x, y, "warning"),
 					},
-				],
-				[
-					rehypeAutolinkHeadings,
-					{
-						behavior: "append",
+				},
+			],
+			[
+				rehypeAutolinkHeadings,
+				{
+					behavior: "append",
+					properties: {
+						className: ["anchor"],
+					},
+					content: {
+						type: "element",
+						tagName: "span",
 						properties: {
-							className: ["anchor"],
+							className: ["anchor-icon"],
+							"data-pagefind-ignore": true,
 						},
-						content: {
-							type: "element",
-							tagName: "span",
-							properties: {
-								className: ["anchor-icon"],
-								"data-pagefind-ignore": true,
-							},
-							children: [{ type: "text", value: "#" }],
-						},
+						children: [{ type: "text", value: "#" }],
 					},
-				],
+				},
 			],
-		}),
+		],
 	},
 	vite: {
 		build: {
